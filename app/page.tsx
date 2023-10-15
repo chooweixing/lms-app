6661b65bce6a3cf46f64924da33c171621788b29
weixing <weixing1991@gmail.com>
2023-10-15 14:30:24 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div className='text-3xl font-medium text-sky-400'>Hello Worl sadasdd</div>
+  )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function test() {
+  return (
+    <div>tessadt</div>
+  )
+}
+
+export default test
